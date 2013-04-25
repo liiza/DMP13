1d68231f127dfae0a16f21f94a8896241120730e
liisa <liisa.sailaranta@aalto.fi>
2013-04-25 20:31:15 +0300

    stash

--- src/com/game/alias/ServerStartedActivity.java
+++ src/com/game/alias/ServerStartedActivity.java
@@ -101,7 +101,7 @@ public class ServerStartedActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.server_started, menu);
+		//getMenuInflater().inflate(R.menu.server_started, menu);
 		return true;
 	}
 
